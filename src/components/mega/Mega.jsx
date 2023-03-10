11526c1ad9f9bc6316a9ada6b4ee2d4bfa2a332e
Bruno Araujo <brunoandradel.a.dev@gmail.com>
2023-03-10 17:01:52 -0300

    Anotações

--- src/components/mega/Mega.jsx
+++ src/components/mega/Mega.jsx
@@ -1,37 +1,42 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 export default props => {
     function gerarNumeroNaoContido(min, max, array) {
         const aleatorio = parseInt(Math.random() * (max + 1 - min)) + min
         return array.includes(aleatorio) ? gerarNumeroNaoContido(min, max, array) : aleatorio
     }
-    
+
     function gerarNumeros(qtde) {
         const numeros = Array(qtde)
             .fill(0)
             .reduce((nums) => {
                 const novoNumero = gerarNumeroNaoContido(1, 60, nums)
-                console.log([ ...nums, novoNumero])
-                return [ ...nums, novoNumero]        
+                console.log([...nums, novoNumero])
+                return [...nums, novoNumero]
             }, [])
             .sort((n1, n2) => n1 - n2)
-     
-            return numeros
+
+        return numeros
     }
-    const qtde = props.qtde || 6
-    const numeorsIniciais = Array(qtde).fill(0)
-    const [numeros, setNumeros] = useState(numeorsIniciais) 
+    const [qtde, setQtde] = useState(props.qtde || 6)
+    const numeorsIniciais = gerarNumeros(qtde)
+    const [numeros, setNumeros] = useState(numeorsIniciais)
 
     return (
         <div>
             <h2>Mega</h2>
             <h3>{numeros.join(' ')}</h3>
             <div>
-                <label></label>
+                <label>Quantidade de Números</label>
+                <input
+                    type="number"
+                    value={qtde}
+                    onChange={e => setQtde(+e.target.value)}
+                />
             </div>
-            <button onClick={ _ => setNumeros(gerarNumeros(qtde))}>Gerar Números</button>
+            <button onClick={_ => setNumeros(gerarNumeros(qtde))}>Gerar Números</button>
         </div>
 
-    ) 
-    
+    )
+
 }
